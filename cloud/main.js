ab8c9fba7389ec340eb191c786c928631f8e43bb
GYOSolutions <tomas.p@gyo-solutions.com>
2016-03-02 18:24:18 -0600

    Added files via upload

--- cloud/main.js
+++ cloud/main.js
@@ -1,48 +1,6 @@
-require('cloud/app.js');
 
-Parse.Cloud.beforeSave("Category", function(request, response) {
-  if (!request.object.id) {
-    console.log("In");
-    var query = new Parse.Query('Category');
-    query.descending('order');
-    query.first({
-      success: function(object) {
-        console.log(object);
-        request.object.set('order', object.get('order')+1);
-        response.success();
-      },
-      error: function() {
-        request.object.set(order, 100);
-        response.success();
-      }
-    });
-  } else {
-    response.success();
-  }
-});
-
-
-Parse.Cloud.beforeSave("Subcategory", function(request, response) {
-  if (!request.object.id) {
-    var query = new Parse.Query('Subcategory');
-    if (request.object.get('category')) {
-      query.equalTo('category', request.object.get('category').objectId)
-    } else {
-      query.equalTo('subcategory', request.object.get('subcategory').objectId)
-    }
-    query.descending('order');
-    query.first({
-      success: function(object) {
-        console.log(object);
-        request.object.set('order', object.get('order')+1);
-        response.success();
-      },
-      error: function() {
-        request.object.set(order, 100);
-        response.success();
-      }
-    });
-  } else {
-    response.success();
-  }
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
 });
